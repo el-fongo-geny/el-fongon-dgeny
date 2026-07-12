5b036f0c8509c502ba2498e7d480ff586aa49ce9
el-fongo-geny <andrisrodriguez2424@gmail.com>
2026-07-11 19:47:19 -0700

    Delete supabase directory

--- supabase/sql/03_enable_realtime.sql
+++
@@ -1,4 +0,0 @@
-alter publication supabase_realtime add table public.orders;
-alter publication supabase_realtime add table public.product_availability;
-
--- Si dice "relation is already member of publication", no pasa nada: ya estaba activado.
